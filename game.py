e5ea62f5276a75bcb4ce852aa6749440ef4bbf25
kerfax2232 <kerfax22@gmail.com>
2016-12-14 21:35:59 -0800

    Added British camp scene

--- game.py
+++ game.py
@@ -12,7 +12,8 @@ class Scene(object):
 class Engine(object):
 
 	#Still unclear on __init__ and self in terms of importance.  Self I believe allows you to call
-	# The variable for any number of objects?  Init is still confusing, but I know it's important and
+	# The variable for any number of objects?  Also is 
+	# implied argument often.  Init is still confusing, but I know it's important and
 	# is called a "constructor" in other languages (Stack Overflow)  and this will not work without it.
     def __init__(self, scene_map):
         self.scene_map = scene_map
@@ -74,44 +75,59 @@ class OpeningScene(Scene):
 class BritishCamp(Scene):
 
     def enter(self):
-    	#Put in storyline/text here to intro scene
-        code = "%d" % (randint(1,5))
-        guess = raw_input ("[keypad]> ")
+    	british_intro = open("british_camp_intro.txt")
+        password = "%d" % (randint(1,5))
+        guess = raw_input ("What is your guess? > ")
         guesses = 0
 
-        while guess != code and guesses < 2:
-            print #Witty response for messing up code
+        #Saying while guess is under this amount of guesses, print/do this
+        while guess != password and guesses < 2:
+            print '"Try again lad/lass."  The general and his guards begin to encircle you, wary of your true identity'  
             guesses += 1
-            guess = raw_input("[keypad]> ")
+            guess = raw_input("What is your guess?> ")
 
-        if guess == code:
-            #put in storyline here for winning the game
+        #If guess is correct
+        if guess == password:
+            print '"We\'ve been waiting for you!  However, we don\'t actually have the map.  Go further into the camp.'
+            print 'You\'ll have to solve a math problem for them to get the map of troop movements"'
             return 'country_saved'
+        
+        #If guess is wrong after specified amount of guesses
         else:
             print #story for failure to win the game
             return 'awake'
 
+#Class for another map scene, this one using addition of some sort
+
 #Put in finishing/winning response
 
+# Class defines the scene names, changing scenes, etc
 class Map(object):
 
     scenes = {
+    	#remember to put the comma at the end genius
         "opening_scene": OpeningScene(),
         "british_camp": BritishCamp(),
+        #getting the map finally
         "awake": Awake(),
-        "country_saved"
+        #"country_saved": CountrySaved(),
     }
+
 
     def __init__(self, start_scene):
         self.start_scene = start_scene
 
+    # sends scene to next specified scene by tying next_scene to getting next scene name
     def next_scene(self, scene_name):
         val = Map.scenes.get(scene_name)
         return val
 
+    #defines opening scene    
     def opening_scene(self):
         return self.next_scene(self.start_scene)
 
+#Starts game at opening scene
 a_map = Map('opening_scene')
+# ties game.play to the 
 a_game = Engine(a_map)
 a_game.play()
